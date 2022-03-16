87f6d0347ba08fc4c1148d12bbdf10cad9b6d6c0
Leszek Michno <leszek.michno@gmail.com>
2022-03-16 07:40:52 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+import "./App.css";
+
+const App = () => {
+  const [option, setOption] = useState("");
+  const [value, setValue] = useState("");
+  const [options, setOptions] = useState(["a", "b", "c"]);
+
+  const handleShowOption = () => {
+    const index = Math.floor(Math.random() * options.length);
+    setOption(options[index]);
+  };
+
+  const handleInputChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  const handleAddOption = () => {
+    if (value === "") return alert("wpisz coś");
+    const omen = options.concat(value); 
+    // const omen = [...options];
+    // omen.push(value);
+    setOptions(omen);
+    setValue('')
+    alert(`dodana wróżba: ${value}`);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={handleShowOption}>Zobacz wróżbę</button>
+      <br />
+      <input type="text" value={value} onChange={handleInputChange} />
+      <button onClick={handleAddOption}>Dodaj wróżbę</button>
+      <br />
+      {option ? <h1>{option}</h1> : null}
     </div>
   );
-}
+};
 
 export default App;
